ba31f1169c0c61ee2b900bb8a4561aab14a5ba55
skraft-redhat <skraft@redhat.com>
2024-08-12 16:00:44 +0200

    is that cool

--- hello-world-golang/hello_openshift.go
+++ hello-world-golang/hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift! NEW"
+		response = "Hello OpenShift! Is that cool!"
 	}
 
 	fmt.Fprintln(w, response)
