88d15a196398501e4dc4e5548fd3b5c345b6b881
skraft-redhat <57144373+skraft-redhat@users.noreply.github.com>
2024-09-02 17:32:41 +0200

    v2.0

--- hello-world-golang/hello_openshift.go
+++ hello-world-golang/hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift! 1.0"
+		response = "Hello OpenShift! 2.0"
 	}
 
 	fmt.Fprintln(w, response)
